447714ba99fb30911267f77283d6abbabcf37bf1
Gunnar Price <pricegunnar@gmail.com>
2023-05-29 14:28:09 -0400

    Updates to world and player

--- player.gd
+++ player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
+const ACCELERATION = 600.0
+const FRICTION = 1000.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -9,20 +11,31 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	apply_gravity(delta)
+	handle_jump()
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	apply_friction(input_axis,delta)
+	handle_acceleration(input_axis,delta)
+
+	move_and_slide()
+
+func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+func handle_jump():
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if is_on_floor(): 
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+			velocity.y = JUMP_VELOCITY / 2
 
-	move_and_slide()
+func apply_friction(input_axis, delta):
+	if input_axis == 0:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
